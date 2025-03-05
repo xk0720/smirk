3f844c2ccb23618c0ddc796da20075d99ce11398
Kevin <xiangyukong0720@gmail.com>
2025-03-05 17:49:23 +0000

    Update repository

--- extract_param_mp.py
+++ extract_param_mp.py
@@ -309,6 +309,7 @@ def inference_worker(model_path: str, input_queue: Queue, output_queue: Queue, g
             # #Method 1:
             video_id, temp_file = data
             frames_batch = torch.load(temp_file)
+            print(f"frames_batch size: {frames_batch.size()}")
             # os.remove(temp_file)  # don't delete at the moment
             print(f"got temp file: {temp_file} for video_id: {video_id}")
 
