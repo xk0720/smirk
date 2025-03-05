84d33a9522a24911877365a272d8076d1c2b550e
Kevin <xiangyukong0720@gmail.com>
2025-03-05 18:32:15 +0000

    Update repository

--- extract_param_mp.py
+++ extract_param_mp.py
@@ -648,8 +648,11 @@ def load_detector():
 
 
 if __name__ == "__main__":
-    mp.set_start_method('spawn', force=True)
+    a = torch.randn(1, 3, 224, 224)
+    torch.save(a, "./tmp/tensor.pt")
+    5/0
 
+    mp.set_start_method('spawn', force=True)
     import argparse
 
     parser = argparse.ArgumentParser(description="Extract 3DMM parameters from videos")
